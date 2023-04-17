2ba75296a6789109c104951356901ca4fbb1d56f
Santhosh642003 <104671626+Santhosh642003@users.noreply.github.com>
2023-04-17 22:36:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(express.static(path.join(__dirname+"/public")));
 
 io.on("connection", function(socket){
     socket.on("newuser", function(username){
-        socket.broadcast.emit("update", username + "joined the conversation");
+        socket.broadcast.emit("update", username + " joined the conversation");
     });
     socket.on("exituser", function(username){
-        socket.broadcast.emit("update", username + "lefy the conversation");
+        socket.broadcast.emit("update", username + " left the conversation");
     });
     socket.on("chat", function(message){
         socket.broadcast.emit("chat", message);
